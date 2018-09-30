35c4e2e3ed81c809c3c82123586933bee82884d3
yinm <yinm.cminor9@gmail.com>
2018-09-30 16:23:45 +0900

    Update using header

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header-container">
+    <ul>
+      <li>INDEX</li>
+      <li>WORKS</li>
+      <li>PUBLICATIONS</li>
+      <li>PROFILE</li>
+      <li>ACCESS&CONTACT</li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
